0df83931e7227f8c7c41d591703b381ae3acc933
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-10-16 16:16:47 +0100

    Update thermo_integ.cpp

--- thermo_integ.cpp
+++ thermo_integ.cpp
@@ -116,7 +116,7 @@ ComputeThermoInteg::ComputeThermoInteg(LAMMPS* lmp, int narg, char** arg) : Comp
         {
             parameter_list |= PAIR;
             pstyle = utils::strdup(arg[iarg + 1]);
-            iarg += 1;
+            iarg += 2;
             lA_ps = new double[ntypeAs];  //NEGATIVE
             lB_ps = new double[ntypeAs];  //POSITIVE
             pparams = new char*[ntypeAs];
@@ -768,70 +768,7 @@ double ComputeThermoInteg::compute_epair_atom()
       for (int i = 0; i < nkspace; i++)
           energy_peratom[i] += eatom[i];
    }
-   /* ----------------------------------------------------------------------
-   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
-   https://www.lammps.org/, Sandia National Laboratories
-   LAMMPS development team: developers@lammps.org
-
-   Copyright (2003) Sandia Corporation.  Under the terms of Contract
-   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-   certain rights in this software.  This software is distributed under
-   the GNU General Public License.
-
-   See the README file in the top-level LAMMPS directory.
-------------------------------------------------------------------------- */
-/* ----  Compute Thermo_Integ written by Mahdi Tavakol (Oxford) mahditavakol90@gmail.com ----
-   ----  For some reasons that I do not know why it does not work with the intel package ---- */
-
-#include "compute_thermo_integ.h"
-
-#include "atom.h"
-#include "comm.h"
-#include "domain.h"
-#include "error.h"
-#include "fix.h"
-#include "force.h"
-#include "input.h"
-#include "kspace.h"
-#include "memory.h"
-#include "modify.h"
-#include "pair.h"
-#include "pair_hybrid.h"
-#include "timer.h"
-#include "update.h"
-#include "variable.h"
-
-using namespace LAMMPS_NS;
-
-enum { SINGLE = 1 << 0, DUAL = 1 << 1 };
-enum {
-    PAIR = 1 << 0,
-    CHARGE = 1 << 1,
-};
-enum {POSITIVE,NEGATIVE};
-
-/* ---------------------------------------------------------------------- */
-
-ComputeThermoInteg::ComputeThermoInteg(LAMMPS* lmp, int narg, char** arg) : Compute(lmp, narg, arg)
-{
-    if (narg < 11) error->all(FLERR, "Illegal number of arguments in compute ti");
-
-
-    peflag = 1;
-    peatomflag = 1;
-    peratom_flag = 1;
-    comm_reverse = 1;
-    
-    
-    scalar_flag = 0;
-    vector_flag = 1;
-    size_vector = 6;
-    peratom_flag = 1; // I need to have per atom energies tallied. 
-    
-    extvector = 0;
-
-    vector = new double[size_vector];
-
+   
    if (force->newton || (force->kspace && force->kspace->tip4pflag)) comm->reverse_comm(this);
    
    
